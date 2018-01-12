d746b9a406699bc2db1e5b4a3e9215957313e565
ronald vilorio <soccer23415@gmail.com>
2018-01-12 04:20:09 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,6 @@ class Show < ActiveRecord::Base
   end
   def self.most_popular_show
     binding.pry
-    self.where("rating = ?",self.maximum(:rating)).title
+    self.where("rating = ?",self.maximum(:rating)).name
   end
 end
