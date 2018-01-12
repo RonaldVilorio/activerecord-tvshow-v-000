f143ea06971492bdd499fe74992ea019d607e5be
ronald vilorio <soccer23415@gmail.com>
2018-01-12 04:27:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -19,7 +19,7 @@ class Show < ActiveRecord::Base
     self.where("rating > 5")
   end
   def self.shows_by_alphabetical_order
-    self.where("SELECT *")
+    self.where("SELECT *").order(:name)
   end
 
 end
