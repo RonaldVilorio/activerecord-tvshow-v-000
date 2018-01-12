30fad3df855db634b5e58d7a4bcd3cbffb8450d8
ronald vilorio <soccer23415@gmail.com>
2018-01-12 04:23:45 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,5 +9,6 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
+  def self.least_popular_show
   # self.where("rating = ?",self.minimum(:rating)).first
 end
